3172bdbfcfdc08b225e3431e6f7bbc2112dd1bd2
Yasith Rashan <154666486+yasithrashan2003@users.noreply.github.com>
2024-07-04 17:36:56 +0530

    SQL Queries 2

---
+++ Queury 3.sql
@@ -0,0 +1,29 @@
+show databases;
+use sql_intro;
+show tables;
+
+create table detials (Name varchar(25),Age int, sex char(1),
+doj date,city varchar(15),salary float);
+
+insert into details values
+("Naruto", 18, "M", "2005-05-30", "Hidden Leaf", 70000),
+("Luffy", 16, "M", "2007-09-17", "Sea", 30000),
+("Sakura", 17, "F", "2005-03-28", "Hidden Leaf", 65000),
+("Zoro", 19, "M", "2004-11-11", "Sea", 60000),
+("Hinata", 18, "F", "2005-12-27", "Hidden Leaf", 68000),
+("Sanji", 18, "M", "2004-03-02", "Sea", 55000),
+("Kakashi", 29, "M", "1994-09-15", "Hidden Leaf", 90000),
+("Gaara", 18, "M", "2005-01-19", "Sand Village", 72000);
+
+show tables;
+
+show databases;
+
+use mysql;
+show tables;
+
+drop table emp_details;
+
+show tables;
+
+
